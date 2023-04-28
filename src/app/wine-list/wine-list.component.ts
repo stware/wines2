597b90fbb8220b482d7a2775e393d91467f71690
stware <stef.turrini@gmail.com>
2023-04-28 17:52:32 +0200

    1 file modified

--- src/app/wine-list/wine-list.component.ts
+++ src/app/wine-list/wine-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { WineService } from '../services/wine.service';
 
 @Component({
   selector: 'app-wine-list',
   templateUrl: './wine-list.component.html',
-  styleUrls: ['./wine-list.component.css']
+  styleUrls: ['./wine-list.component.css'],
 })
 export class WineListComponent implements OnInit {
+  constructor(private _wineService: WineService) {}
 
-  constructor() { }
+  wines: any[] = [];
 
   ngOnInit() {
-  }
+    this._wineService.getWines().subscribe((data) => {
+      this.wines = [];
+      data.forEach((element: any) => {
+        // acceder a los id de la base
+        // console.log(element.payload.doc.id);
+        // acceder a la informacion
+        // console.log(element.payload.doc.data());
 
+        // objeto con id e informacion
+        this.wines.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data(),
+        });
+      });
+      console.log(this.wines);
+    });
+  }
 }
